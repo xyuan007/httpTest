31b2572fce621aeabaa7db2e3ba3b4144c768574
xyuan007 <85224271@qq.com>
2016-03-01 11:29:21 +0800

    create project

---
+++ src/main/java/com/creditease/xyuan/httpTest/App.java
@@ -0,0 +1,13 @@
+package com.creditease.xyuan.httpTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
