9ec48b34b37c41eb7798c33f1a675c5847109928
xyuan007 <85224271@qq.com>
2016-05-27 15:08:04 +0800

    push

---
+++ src/main/java/com/creditease/xyuan/httpTest/Assert/newsmthlogin.java
@@ -0,0 +1,24 @@
+package com.creditease.xyuan.httpTest.Assert;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import net.sf.json.JSONObject;
+import com.creditease.xyuan.httpTest.Helper.DataHelper;
+
+public class newsmthlogin {
+	private DataHelper dh = null;
+	private JSONObject header = null;
+	private JSONObject json = null;
+	
+	public newsmthlogin(String response) throws Exception{
+//		dh = new DataHelper();
+//		
+//		json = JSONObject.fromObject(response);
+//		header = JSONObject.fromObject(json.get("header"));
+	}
+	
+	public void login() {
+	}
+	
+	
+}
